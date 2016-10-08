70107d2ed6b0c5776a66cb69a684382b71874e3b
Anton Vakhrushev <anwinged@ya.ru>
2016-10-08 10:41:15 +0300

    Improve validation for url

--- src/Claw/Service/SearchRequestValidator.php
+++ src/Claw/Service/SearchRequestValidator.php
@@ -9,6 +9,29 @@ use Claw\Entity\SearchType;
 
 class SearchRequestValidator
 {
+    /**
+     * Шаблон для проверки URL.
+     *
+     * @link https://github.com/symfony/validator/blob/master/Constraints/UrlValidator.php
+     */
+    const URL_PATTERN = '~^
+            (http|https)://                         # protocol
+            (
+                ([\pL\pN\pS-\.])+(\.?([\pL\pN]|xn\-\-[\pL\pN-]+)+\.?) # a domain name
+                    |                                                 # or
+                \d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}                    # an IP address
+            )
+            (:[0-9]+)?                              # a port (optional)
+            (/?|/\S+|\?\S*|\#\S*)                   # a /, nothing, a / with something, a query or a fragment
+        $~ixu';
+
+    /**
+     * Проверяет перенаддный запрос поиска.
+     *
+     * @param SearchRequest $searchRequest
+     *
+     * @return array
+     */
     public function validate(SearchRequest $searchRequest): array
     {
         $errors = [];
@@ -17,7 +40,7 @@ class SearchRequestValidator
         $type = $searchRequest->getType();
         $text = $searchRequest->getText();
 
-        if (filter_var($url, FILTER_VALIDATE_URL) === false) {
+        if (!preg_match(self::URL_PATTERN, $url)) {
             $errors['url'] = 'Адрес указан в неправильном формате';
         }
 
